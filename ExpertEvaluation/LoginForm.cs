fd7545d0440de313ed1a21b79a1abfa1959c8e58
Dmitry Vinokurov <dvinokurov777@gmail.com>
2014-03-01 22:18:44 +0200

    first commit

---
+++ ExpertEvaluation/LoginForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpertEvaluation
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            string login = loginTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            //TODO: Add login logic 
+
+            this.Hide();
+            var adminForm = new AdminForm();
+            adminForm.Show();
+
+        }
+    }
+}
