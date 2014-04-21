c4c4f05cc2a5faafed557d7bdd4278db0991c9df
UinaGriffin <marinapopova05@gmail.com>
2014-04-21 23:13:44 +0300

    in progress questionary form

--- ExpertEvaluation/forms/QuestionaryForm.cs
+++ ExpertEvaluation/forms/QuestionaryForm.cs
@@ -18,23 +18,88 @@ namespace ExpertEvaluation.forms
         public QuestionaryForm()
         {
             InitializeComponent();
+            InitializeQuestionPanelDictionary();
             InitializeQuestionsLV();
         }
+        private static Dictionary<QuestionType, Panel> _answerPanelDictionary;
+        private void InitializeQuestionPanelDictionary()
+        {
+            _answerPanelDictionary = new Dictionary<QuestionType, Panel>()
+            {
+                {QuestionType.BooleanQuestion, booleanAP},
+                {QuestionType.OneOfMany, manyOfManyAP},
+                {QuestionType.NumberQuestion, numberAP},
+                {QuestionType.ManyOfMany, manyOfManyAP},
+                {QuestionType.Interval, intervalAP}
+
+                // TODO: add Fuzzy interval mapping
+            };
+        }
+
 
         private void questionsLV_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var selectedIndex = questionsLV.SelectedIndices[0];
-            var selectedId = int.Parse(questionsLV.Items[selectedIndex].Text);
-            _selectedQuestion = Dao.GetQuestionByNumber(selectedId);
-            RedrawQuestion();
+            int selectedId = 2;
+            try
+            {
+                var selectedIndex = questionsLV.SelectedIndices[0];
+                selectedId = int.Parse(questionsLV.Items[selectedIndex].Text);
+            }
+            catch (Exception exception)
+            {}
+                _selectedQuestion = Dao.GetQuestionByNumber(selectedId);
+                RedrawQuestion();
+            
         }
 
         private void RedrawQuestion()
         {
             questionNumberLabel.Text = @"Question #" + _selectedQuestion.QuestionNumber;
             questionTextRTB.Text = _selectedQuestion.QuestionText;
-            // TODO: add output for question-type-specific data here
+
+            PrepareTypeSpecificData();
         }
+
+        private void PrepareTypeSpecificData()
+        {
+            SelectAnswerPanel();
+            switch (_selectedQuestion.QSuestionType)
+            {
+
+                case QuestionType.OneOfMany:
+                    manyOfManyCLB.SelectionMode = SelectionMode.One;
+                    manyOfManyCLB.Items.Clear();
+                    manyOfManyCLB.Items.AddRange(items: _selectedQuestion.PossibleAnswers.ToArray());
+                    foreach (var rightAnswer in _selectedQuestion.RightAnswers)
+                    {
+                        var index = manyOfManyCLB.Items.IndexOf(rightAnswer);
+                        manyOfManyCLB.SetItemChecked(index, true);
+                    }
+                    break;
+                case QuestionType.ManyOfMany:
+                    manyOfManyCLB.SelectionMode = SelectionMode.MultiSimple;
+                    manyOfManyCLB.Items.Clear();
+                    manyOfManyCLB.Items.AddRange(items: _selectedQuestion.PossibleAnswers.ToArray());
+                    foreach (var rightAnswer in _selectedQuestion.RightAnswers)
+                    {
+                        var index = manyOfManyCLB.Items.IndexOf(rightAnswer);
+                        manyOfManyCLB.SetItemChecked(index, true);
+                    }
+                    break;
+            }
+        }
+        
+        private void SelectAnswerPanel()
+        {
+            foreach (var panel in _answerPanelDictionary.Values)
+            {
+                panel.Visible = false;
+            }
+            var selectedPanel = _answerPanelDictionary[_selectedQuestion.QuestionType];
+            selectedPanel.Visible = true;
+        }
+
+
         
         private void QuestionaryForm_Load(object sender, EventArgs e)
         {
