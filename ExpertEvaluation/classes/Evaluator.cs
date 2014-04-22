5836d77caa0e6d9128362c7ccb46968fcc2ff70b
dvinokurov <dmytro.vynokurov@gmail.com>
2014-04-22 20:39:44 +0300

    Evaluator class introduced

---
+++ ExpertEvaluation/classes/Evaluator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpertEvaluation.classes
+{
+    class Evaluator
+    {
+        private User _expert;
+        private readonly Answer[] _answers;
+
+        public Evaluator(User expert)
+        {
+            if (expert.Type != UserType.Expert) 
+                throw new ArgumentException("We can evaluate Experts only");
+            _expert = expert;
+            _answers = expert.Answers;
+        }
+
+        public double Evaluate()
+        {
+            var tMatrices = GetTMatrices();
+            return GetAggregatedEvaluation(tMatrices);
+        }
+
+        private List<TMatrix> GetTMatrices()
+        {
+            var result = GetTMatricesForNonBooleanQuestions();
+            var booleanTMatrix = GetTMatrixForBooleanQuestions();
+            result.Add(booleanTMatrix);
+
+            return result;
+
+        }
+
+        private List<TMatrix> GetTMatricesForNonBooleanQuestions()
+        {
+            var otherAnswers =
+                (List<Answer>)_answers.Where(answer => answer.QuestionType != QuestionType.BooleanQuestion);
+            throw new NotImplementedException();
+        }
+
+        private TMatrix GetTMatrixForBooleanQuestions()
+        {
+            var boolAnswers =
+                (List<Answer>)_answers.Where(answer => answer.QuestionType == QuestionType.BooleanQuestion);
+            throw new NotImplementedException();
+        }
+
+        private double GetAggregatedEvaluation(List<TMatrix> tMatrices)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
