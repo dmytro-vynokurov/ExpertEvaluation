975fd7120dad0ce8f2b7e1dd51cb3d313c9b1b12
dvinokurov <dmytro.vynokurov@gmail.com>
2014-06-17 01:30:16 +0300

    refactored to match new requirements

---
+++ ExpertEvaluation/forms/QuestionTypePickerForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExpertEvaluation.classes;
+
+namespace ExpertEvaluation.forms
+{
+    public partial class QuestionTypePickerForm : Form
+    {
+        private List<String> _questionTypeNames; 
+
+        public QuestionTypePickerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_questionTypeNames.Contains(comboBox1.Text))
+            {
+                MessageBox.Show(@" Please choose question type from the list", @"Wrong question type",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var questionType = Question.GetByName(comboBox1.Text);
+            this.Hide();
+            var answerForm = new AnswerForm(questionType);
+            answerForm.Show();
+        }
+
+        private void QuestionTypePicker_Load(object sender, EventArgs e)
+        {
+            _questionTypeNames = new List<string>();
+            foreach (var pair in Question.QuestionDictionary)
+            {
+                comboBox1.Items.Add(pair.Value);
+                _questionTypeNames.Add(pair.Value);
+            }
+        }
+    }
+}
